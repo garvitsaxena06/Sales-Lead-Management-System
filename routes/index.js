b4188200fe166bbce1172890abfdbc72722b8404
Garvit Saxena <garvitsaxena06@gmail.com>
2020-10-09 17:15:26 +0530

    admin check added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 let user = require('../controllers/user')
 
+let { isLoggedIn, hasAuth } = require('../middleware/hasAuth')
+
 // GET login page
 router.get('/login', user.show_login)
 
@@ -29,21 +31,21 @@ router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead)
 
 // GET leads list
-router.get('/leads', landing.show_leads)
+router.get('/leads', hasAuth, landing.show_leads)
 
 // GET particular lead details
-router.get('/lead/:lead_id', landing.show_lead)
+router.get('/lead/:lead_id', hasAuth, landing.show_lead)
 
 // GET edit a lead's details page
-router.get('/lead/:lead_id/edit', landing.show_edit_lead)
+router.get('/lead/:lead_id/edit', hasAuth, landing.show_edit_lead)
 
 // Submit edit lead's details
-router.post('/lead/:lead_id/edit', landing.edit_lead)
+router.post('/lead/:lead_id/edit', hasAuth, landing.edit_lead)
 
 // Deleting a lead's details
-router.post('/lead/:lead_id/delete', landing.delete_lead)
+router.post('/lead/:lead_id/delete', hasAuth, landing.delete_lead)
 
 // Deleting a lead's details --AJAX method
-router.post('/lead/:lead_id/delete-json', landing.delete_lead_json)
+router.post('/lead/:lead_id/delete-json', hasAuth, landing.delete_lead_json)
 
 module.exports = router;
